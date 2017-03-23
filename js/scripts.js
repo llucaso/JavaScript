85764c3e8e9a520c41094be388db3d5b23259634
Lukasz Redzinski <Lukasz Redzinski>
2017-03-23 19:51:06 +0100

    simple mathematical operation

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+﻿alert('(a * a) + (2 * a * b) - (b * b)');
+var a = prompt('podaj wartość a');
+var b = prompt('podaj wartość b');
+var value = (a * a) + (2 * a * b) - (b * b);
+alert(value);
+
+console.log('wynik działania ' +  value);
+
+if ( value > 0 ) {
+	console.log('wynik dodatni');
+} else if ( value < 0 ) {
+	console.log('wynik ujemny');
+} 
+else {
+	console.log('zero');
+}
+
+
