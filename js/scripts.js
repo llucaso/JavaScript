e6b6fce3742608190b02e4235b32a6cdbf844b7b
llucaso <lredzinski@gmail.com>
2017-03-26 13:22:41 +0200

    local commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-﻿alert('(a * a) + (2 * a * b) - (b * b)');
-var a = prompt('podaj wartość a');
-var b = prompt('podaj wartość b');
-var value = (a * a) + (2 * a * b) - (b * b);
-alert(value);
-
-console.log('wynik działania ' +  value);
-
-if (value == 0) {
-	console.log('zero');
-} else {
-	(value > 0) ? console.log('wynik dodatni') : console.log('wynik ujemny')
+﻿function getTriangleArea(a,h) {
+	var wynik;
+	if ( (a <= 0) || (h <= 0)) {
+		wynik = 'Nieprawidłowe dane';
+	} else {
+		wynik = a*h/2;
+	}
+	return wynik;
 }
+
+var triangle1Area = getTriangleArea(10,15);
+var triangle2Area = getTriangleArea(-1,25);
+var triangle3Area = getTriangleArea(10,10);
+
+
+console.log(getTriangleArea(10,6));
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
