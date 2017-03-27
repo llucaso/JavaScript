69e6f0996f200a3e686656939af267e0d8311e9f
llucaso <lredzinski@gmail.com>
2017-03-27 22:16:27 +0200

    String

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+﻿var txt = 'Ala ma kota, kot ma Alę';
+var indexKota = txt.indexOf('kot');
+
+if (indexKota == -1) {
+  console.log('kot nie występuje w tekście');
+} else {
+  console.log(txt[indexKota]);
+}
