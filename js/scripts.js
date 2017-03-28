1142a9408aa2bedef1ef593df13dfc47461ea473
llucaso <lredzinski@gmail.com>
2017-03-28 19:04:53 +0200

    improvement

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 ﻿var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 var animal = 'Zielone słonie';
+var textTable = text.split(' ');
 var animalUpperCased = animal.toUpperCase();
-var textCharsAfter = text.replace('Papugi', animalUpperCased);
-var newCharsAfter = textCharsAfter.substr(0, 45);
+var textCharsAfter = text.replace(textTable[0], animalUpperCased);
 
-console.log(newCharsAfter);
+
+
+console.log(textCharsAfter.substr(0, textCharsAfter.length / 2));
 
 
 
