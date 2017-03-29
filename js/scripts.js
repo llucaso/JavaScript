a466c046f778b7c1b3e0de1b402950cb26b83f51
Lukasz Redzinski <Lukasz Redzinski>
2017-03-29 11:27:50 +0200

    tree pattern

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+﻿function rysujChoinke(star) {
+	for (var k = 1; k <= star; k++) {
+		var stars=" ";
+		for (var i = 0; i < k * 2 - 1; i++) {
+			stars += "*";
+		}
+		console.log(stars);
+		stars = ' '; 
+		}
+}
+rysujChoinke(5);
