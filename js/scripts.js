873b3e6471b59181aff18a31c5a6459aaa5468ea
llucaso <lredzinski@gmail.com>
2017-04-01 18:51:22 +0200

    add new item function

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+﻿var element = document.getElementById('addElem');
+var list = document.getElementById('Lista');
+var number = 1;
+
+
+
+element.addEventListener('click', function(e) {
+
+	var newLi = document.getElementById('list');
+	var newElem = document.createElement('li');
+	newElem.innerHTML = 'item' + number++;
+	newLi.appendChild(newElem);}
+);
+
+
+
+
+
+
